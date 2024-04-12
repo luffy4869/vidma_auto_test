fdaf93fb171b705bef27c0b22d98bba28bbd2f0f
luffy4869 <wy13076768756@163.com>
2024-04-12 17:51:06 +0800

    update code

--- Player/click_next_video.py
+++ Player/click_next_video.py
@@ -6,8 +6,6 @@ d = u2.connect()
 d.app_start("vidma.mkv.xvideo.player.videoplayer.free")
 sleep(5)
 d.xpath('//*[@resource-id="vidma.mkv.xvideo.player.videoplayer.free:id/videoRV"]/android.view.ViewGroup[1]').click()
-# while True:
-#     sleep(2)
-#     d(resourceId="vidma.mkv.xvideo.player.videoplayer.free:id/iv_seek_forward").click()
-# sleep(2)
-d(resourceId="vidma.mkv.xvideo.player.videoplayer.free:id/ivVMore").click()
+while True:
+    sleep(2)
+    d(resourceId="vidma.mkv.xvideo.player.videoplayer.free:id/iv_seek_forward").click()
